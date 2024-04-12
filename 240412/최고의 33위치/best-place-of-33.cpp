fa5d4238cf35d64693461ebb50e185fd4f22e81b
coolfin <sorlti6952@gmail.com>
2024-04-12 15:14:46 +0900

    Title: 최고의 33위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240412/최고의 33위치/best-place-of-33.cpp
+++ 240412/최고의 33위치/best-place-of-33.cpp
@@ -20,21 +20,20 @@ int main() {
     int res = 0;
     while (true) {
         int total =0;
-        for(int i = 0; i< 3; i++) {
-            for(int j = 0; j < 3; j++) {
+        for(int i = r_pos; i< r_pos+3; i++) {
+            for(int j = c_pos; j < c_pos+3; j++) {
                 if (arr[i][j]) total += 1;
             }
         }
 
         res = max(res, total);
-        
+
         c_pos += 1;
-        if(c_pos == N) {
+        if(r_pos == N-3 && c_pos > N-3) break;
+        if(c_pos == N-2) {
             c_pos = 0;
             r_pos += 1;
         }
-
-        if(r_pos == N-3 && c_pos > N-3) break;
     }
     
     cout << res;
