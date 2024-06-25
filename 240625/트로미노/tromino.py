922ee59de2b0536c7f6d28cde802c35f516f4622
coolfin <sorlti6952@gmail.com>
2024-06-25 19:31:13 +0900

    Title: 트로미노, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240625/트로미노/tromino.py
@@ -0,0 +1,35 @@
+import sys
+read = sys.stdin.readline
+
+N, M = map(int, read().rstrip().split())
+arr = [list(map(int, read().rstrip().split())) for _ in range(N)]
+arr_90 = list(zip(*arr))
+
+res = float('-inf')
+
+#2번블럭
+for i in range(N):
+    for j in range(N-M+1):
+        row_total = sum(arr[i][j:j+M])
+        col_total = sum(arr_90[i][j:j+M])
+
+        res = row_total if res < row_total else res
+        res = col_total if res < col_total else res
+
+#1번블럭 (부분행렬)
+for i in range(N - 1):
+    tar = []
+    for j in range(N - 1):
+        tar.append(arr[i][j])
+        tar.append(arr[i][j+1])
+        tar.append(arr[i+1][j])
+        tar.append(arr[i+1][j+1])
+
+        tar.sort()
+        tar.pop(0)
+
+        square_res = sum(tar)
+        res = square_res if res < square_res else res
+        tar.clear()
+
+print(res)
