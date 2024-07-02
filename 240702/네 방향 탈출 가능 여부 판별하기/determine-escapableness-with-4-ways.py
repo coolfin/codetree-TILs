1010662c70b893f0f421ba503ad3a41f11c4c71b
coolfin <sorlti6952@gmail.com>
2024-07-02 12:54:19 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 1681ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240702/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240702/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -12,13 +12,9 @@ dy = [1, 0, -1, 0]
 deq = deque()
 deq.append([0,0])
 
-res = 0
 while deq:
     x, y = deq.popleft()
     visited[x][y] = True
-    if x == N-1 and y == M-1:
-        res = 1
-        break
 
     for i in range(4):
         cx = x+dx[i]
@@ -26,6 +22,9 @@ while deq:
         
         if cx >= N or cy >= M or cx < 0 or cy < 0 or not arr[cx][cy] or visited[cx][cy]: continue
 
+        if cx == N-1 and cy == M-1:
+            print(1)
+            exit()
         deq.append([cx,cy])
 
-print(res)
+print(0)
